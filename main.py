7ab60a8f9873b63b3847291214c42b23cd472f08
root <root@kali>
2025-06-09 08:12:28 -0400

    Update

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import os
 from fastapi import FastAPI, Form
 from utils import login_and_save_session, like_and_comment_on_posts
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 @app.post("/login/")
 def login(
@@ -11,8 +17,14 @@ def login(
 ):
     result = login_and_save_session(username, password, verification_code=code)
     return result
+    
 
 @app.post("/like_comment/")
 def do_actions(username: str = Form(...)):
     result = like_and_comment_on_posts(username)
     return result
+    
+    
+@app.get("/", response_class=HTMLResponse)
+async def show_panel(request: Request):
+    return templates.TemplateResponse("panel.html", {"request": request})
